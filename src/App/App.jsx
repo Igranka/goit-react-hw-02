788b2ff165708ca10189302bcf936fc568fb300b
Igranka <sirbusiness0@gmail.com>
2024-08-20 14:25:03 +0300

    0.2.3 test

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,24 +1,24 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 function App() {
-  const [options, setOptions] = useState(0);
-  const [feedback, setFeedback] = useState(() => {
-      const savedFeedback = window.localStorage.getItem("saved-feedback");
-      if (savedFeedback !== null) {
-        return JSON.parse(savedFeedback);
-      }
-      return { good: 0, neutral: 0, bad: 0 };
-  });
+  // const [options, setOptions] = useState(0);
+  // const [feedback, setFeedback] = useState(() => {
+  //     const savedFeedback = window.localStorage.getItem("saved-feedback");
+  //     if (savedFeedback !== null) {
+  //       return JSON.parse(savedFeedback);
+  //     }
+  //     return { good: 0, neutral: 0, bad: 0 };
+  // });
 
-  const handleFeedback = () => {
-		setFeedback(feedback + 1);
-  };
+  // const handleFeedback = () => {
+	// 	setFeedback(feedback + 1);
+  // };
 
-  return (
-    <div className="yes">
-      Hello
-    </div>
-  );
+  // return (
+  //   <div className="yes">
+  //     Hello
+  //   </div>
+  // );
 }
 export default App
 
